dace1b6e5d922951abbeefdf78e2c87a1690501b
Sergio Ocón-Cárdenas <sergio.ocon@redhat.com>
2021-01-06 13:24:15 +0100

    error in builds.sh file using vi

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 # Change the name for whatever fits you so it can be uploded to quay with your username
 
 # buildah bud -t <image-name>
-buildah bud -t quay.io/chargio/phoenix-test
+IMAGE_TAG=quay.io/chargio/phoenix-test
+MIX_ENV=prod buildah bud -t $IMAGE_TAG
